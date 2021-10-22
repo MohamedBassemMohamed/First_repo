0e761b699f44fd169b28cb375c2c67b1dc3744fd
“Mohamed <mobasem369@gmail.com>
2021-10-22 22:03:18 +0200

    My first commit

---
+++ NetBeansProjects/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,21 @@
+
+package javaapplication1;
+
+ import java . util . Scanner;
+public class JavaApplication1 {
+
+       public static void main(String[] args) {
+        // TODO code application logic here
+       // System .out .println("he said that\n \"i am from egypt \" and\n   \"he is from usa\" ");
+         //  System.out.println("the file located in d:\t javaprojects\tbbeesssoooo");
+         Scanner input = new Scanner(8,9);
+         int num1=input.nextInt();
+         int num2=input.nextInt();
+         int sum = num1 + num2;
+           System.out.println("sum =\t" + sum);
+         
+         
+         
+    }
+    
+}
